d22c20eacf18c98f182938915ae2234495a29f19
SMTejas <smtejasgowda@gmail.com>
2025-11-11 00:13:09 +0530

    Changes in backend for render deployment

--- routes/upload.js
+++ routes/upload.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const upload = require('../middleware/upload');
+// const upload = require('../middleware/upload');
 const { authenticateToken } = require('../middleware/auth');
 const { uploadFile, getUserFiles, getFileData, deleteFile } = require('../controllers/uploadController');
 const multer = require('multer');
